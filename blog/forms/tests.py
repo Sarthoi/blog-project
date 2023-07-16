10679c40dd21436d589851d755d1847ea5637789
Sarthoi <saadrak.oai@gmail.com>
2023-07-16 18:51:11 -0400

    Ver 1.2

--- blog/forms/tests.py
+++ blog/forms/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from forms.models import *
 
 # Create your tests here.
+
+class GamesModelTestCase(TestCase):
+    def setUp(self):
+        self.game = Games.objects.create(
+            nombre="Juego de Prueba",
+            descripcion="Descripción del juego de prueba",
+            precio=50,
+            stock=100,
+            plataforma=1,
+            imagen=None,
+        )
+
+    def test_nombre_max_length(self):
+        max_length = self.game._meta.get_field('nombre').max_length
+        self.assertEqual(max_length, 50)
+
+    def test_precio_positive(self):
+        self.assertGreaterEqual(self.game.precio, 0)
+
+    def test_imagen_upload_to(self):
+        if self.game.imagen:
+            self.assertTrue(self.game.imagen.name.startswith('games/'))
--- blog/login/models.py
+++ blog/login/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Message(models.Model):
+    sender = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 
 class AvatarModel(models.Model):
     user= models.ForeignKey(User, on_delete=models.CASCADE)
